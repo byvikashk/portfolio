be5bd29fc05250b95c83002cd323d7c47803c59e
VIKASH KUMAR <95743688+Vikashcpr@users.noreply.github.com>
2026-07-04 22:38:49 +0530

    git commit -m "git commit -m "Initial commit: Personal Portfolio Website""

---
+++ Portfolio/wwwroot/js/site.js
@@ -0,0 +1,67 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+
+    const navbar = document.querySelector(".custom-navbar");
+    const collapse = document.getElementById("navbar");
+
+    document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+        link.addEventListener("click", function (e) {
+
+            e.preventDefault();
+
+            const target = document.querySelector(this.getAttribute("href"));
+
+            if (!target) return;
+
+            const scrollToSection = () => {
+
+                // Wait for collapse animation to finish
+                setTimeout(() => {
+
+                    const navbarHeight = navbar.offsetHeight;
+
+                    const top =
+                        target.getBoundingClientRect().top +
+                        window.pageYOffset -
+                        navbarHeight;
+
+                    window.scrollTo({
+                        top: top,
+                        behavior: "smooth"
+                    });
+
+                }, 350);
+
+            };
+
+            // Mobile Menu Open
+            if (collapse.classList.contains("show")) {
+
+                const bsCollapse = bootstrap.Collapse.getOrCreateInstance(collapse);
+
+                collapse.addEventListener("hidden.bs.collapse", scrollToSection, {
+                    once: true
+                });
+
+                bsCollapse.hide();
+            }
+            else {
+
+                const navbarHeight = navbar.offsetHeight;
+
+                const top =
+                    target.getBoundingClientRect().top +
+                    window.pageYOffset -
+                    navbarHeight;
+
+                window.scrollTo({
+                    top: top,
+                    behavior: "smooth"
+                });
+            }
+
+        });
+
+    });
+
+});
